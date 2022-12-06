6c2525e8215c93f2cae7ec2b5b125320da0f9847
zia <zia166@gmail.com>
2022-12-06 14:40:42 +0000

    ### Enhancement 7 Enable priority to an item

--- todo.py
+++ todo.py
@@ -14,12 +14,12 @@ class Handler:
         parser = argparse.ArgumentParser()
         parser.add_argument(
             "action",
-            choices=["add", "list", "delete", "do", "done"],
+            choices=["add", "list", "delete", "do", "done", "pri"],
             help="The action to take",
         )
         parser.add_argument("other", nargs="?")
-        args = parser.parse_args()
 
+        args = parser.parse_args()
         action = getattr(self, args.action)
         action()
 
@@ -37,40 +37,53 @@ class Handler:
         )
         parser.add_argument(
             "search",
-            default=None,
+            default="None",
             type=hyphenated,
         )
-        args = parser.parse_args()
-        print(args.action)
+
+        try:
+            args, unknown = parser.parse_known_args()
+        except SystemExit:
+            args = parser.parse_args(["action"])
+            pass
+
         if args.action is None:
             with open(self.todo_file, "r") as f:
                 items = f.readlines()
                 item1, item2 = [], []
                 index1, index2 = [], []
+                item = []
                 for i, line in enumerate(
                     items, start=1
                 ):  ##Enhancement 2 The entries are listed First entry 1
 
-                    if line.startswith(
-                        "("
-                    ):  ### Enhancement 6 ..List items by priority.
+                    if line.startswith("("):
+                        ### Enhancement 6 ..List items by priority.
                         result = line[line.find("(") + 1 : line.find(")")]
-                        item1.append(line)
-                        index1.append(i)
-                        item1 = sorted(item1)
+                        ## To sort only with priorty letter
+                        if len(result) <= 1:
+                            item1.append(line)
+                            item1 = sorted(item1)
+                        else:
+                            item2.append(line)
+                            # index2.append(i)
                     else:
                         item2.append(line)
-                        index2.append(i)
+                        # index2.append(i)
 
-                i = 0
-                for line in item1:
-                    ##Enhancement 3 Prevent different indentation
-                    print(f"{index1[i]:{2 if i<=9 else 0}} {line}")
-                    i = i + 1
-                i = 0
-                for val in item2:
-                    print(f"{index2[i]:{2 if i<=9 else 0}} {val}")
-                    i = i + 1
+            ##Enhancement 3 Prevent different indentation
+            for line in item1:
+                if line in items:
+                    print(
+                        f"{items.index(line)+1:{2 if items.index(line)+1<=9 else 0}} {line.strip()} "
+                    )
+
+            for line in item2:
+                if line in items:
+                    print(
+                        f"{items.index(line)+1:{2 if items.index(line)+1<=9 else 0}} {line.strip()}"
+                    )
+
         else:
             with open(self.todo_file, "r") as f:
                 items = f.readlines()
@@ -100,6 +113,7 @@ class Handler:
         parser.add_argument("action", choices=["add"])
         parser.add_argument("item", type=singleLine)
         args = parser.parse_args()
+        # print(args.item)
         with open(self.todo_file, "a") as f:
             f.write(args.item + "\n")
 
@@ -138,7 +152,7 @@ class Handler:
         # Write out all but the done items
         with open(self.todo_file, "w") as f:
             new_todos = "".join(
-                items[: args.line_number] + items[args.line_number + 1 :]
+                items[: args.line_number - 1] + items[args.line_number :]
             )
             f.write(new_todos)
 
@@ -161,6 +175,37 @@ class Handler:
                     if line != args.item:
                         f.write(data)
         print(f"Deleted: {items[args.item -1].strip()}")
+
+    ### Enhancement 7 Enable assigning a priority to an item:
+    def pri(self):
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("pri", choices=["pri"])
+        parser.add_argument("line_number")
+        args = parser.parse_args()
+
+        with open(self.todo_file, "r+") as f:
+            items = f.readlines()
+            val = int(args.line_number[0]) - 1
+
+            for line, data in enumerate(items):
+                if line == val:
+                    if data.startswith("("):
+                        print(f"({args.line_number[1]}) {data[4:].strip()}")
+                        items[line] = (
+                            f"({args.line_number[1]})" + " " + data[4:].strip() + "\n"
+                        )
+                        print("yes")
+                    else:
+                        print(f"({args.line_number[1]}) {data.strip()}")
+                        items[line] = (
+                            f"({args.line_number[1]})" + " " + data.strip() + "\n"
+                        )
+                        print("NO")
+
+            f.seek(0)
+            for data in items:
+                f.write(data)
 
 
 if __name__ == "__main__":
